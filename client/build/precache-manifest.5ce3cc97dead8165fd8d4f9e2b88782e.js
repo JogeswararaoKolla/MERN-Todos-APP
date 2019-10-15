25ae6810a046c481f236ad579ade3528febed9e1
Jogeswararao Kolla <jogeskolla@gmail.com>
2019-10-15 00:07:32 -0700

    npm run build for client folder

---
+++ client/build/precache-manifest.5ce3cc97dead8165fd8d4f9e2b88782e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "708980a8d056dabae96d4cac25e3433c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f78b8541c53ed6273b5",
+    "url": "/static/js/2.12e28501.chunk.js"
+  },
+  {
+    "revision": "c77f5abde699582cab58",
+    "url": "/static/js/main.46eb7e01.chunk.js"
+  },
+  {
+    "revision": "cbee8d858c084296ccff",
+    "url": "/static/js/runtime-main.985e6003.js"
+  }
+]);
