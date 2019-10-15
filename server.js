0079af96eec311705e2badd52756ce703185268a
Jogeswararao Kolla <jogeskolla@gmail.com>
2019-10-15 00:00:36 -0700

    updated the static app path  for production

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 app.use(routes);
 
 app.listen(PORT, function() {
